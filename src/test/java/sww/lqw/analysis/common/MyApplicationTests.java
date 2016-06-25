dbc8e8f3052e53146f229b108cb95942d361faa0
quanwei.lqw <quanwei.lqw@alibaba-inc.com>
2016-06-25 18:52:00 +0800

    init

---
+++ src/test/java/sww/lqw/analysis/common/MyApplicationTests.java
@@ -0,0 +1,16 @@
+package sww.lqw.analysis.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MyApplication.class)
+public class MyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
